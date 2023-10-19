896ec77b4c571380d87ee71483630e4f6345abaf
Matthew <nohumanman@gmail.com>
2023-10-19 07:33:24 +0100

    Fixed file error

--- server.py
+++ server.py
@@ -1,9 +1,3 @@
-import sys
-sys.path.insert(0, '/var/www/GOVLITE.UK')
-
-from server import application
-
-admin@ip-172-26-14-70:/var/www/GOVLITE.UK$ cat server.py
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="assets")
