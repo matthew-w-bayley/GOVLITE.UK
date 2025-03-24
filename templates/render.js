49da01e132982187a2abf1cc9064c05370a85e4e
Matthew Bayley <nohumanman@gmail.com>
2025-03-24 01:13:00 +0000

    Update render.js to pass base URL to nunjucks render function

--- templates/render.js
+++ templates/render.js
@@ -9,14 +9,13 @@ const env = new nunjucks.Environment(new nunjucks.FileSystemLoader('.'), {
 });
 
 const BASE_URL = "/GOVLITE.UK/";
-env.addGlobal('base_url', BASE_URL);
 
 async function ls(path) {
   const dir = await fs.promises.opendir(path)
   for await (const dirent of dir) {
     var filename = dirent.name;
     if (filename.slice(filename.length - 3) == "njk"){
-      var res = nunjucks.render(filename);
+      var res = nunjucks.render(filename, { base_url: BASE_URL });
       fs.writeFile(filename.slice(0, filename.length-4) + ".html", res, err => {if (err) {console.error(err);}});
     }
   }
